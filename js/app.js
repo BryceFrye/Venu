dba57206ddee7351a479cdb4c3134445e1db9e88
mcole619 <mcole619@gmail.com>
2012-06-23 17:40:59 -0700

    still kinda messed up, but minimally functionail

--- js/app.js
+++ js/app.js
@@ -4,6 +4,108 @@ $('#search-artist input').keypress(function(e) {
   }
 });
 
+function getData() {
+	$(".ui-autocomplete").hide();
+	$("header").slideUp();    
+  $('#artist-name').html("");
+	$('#notice').html("");
+  $('#shows').html("");
+	var origArtist = $('#search-artist input').val();
+  var artist = toTitleCase(origArtist);
+  $.ajax({
+    url: "http://ws.audioscrobbler.com/2.0/?method=artist.getevents&artist="+artist+"&autocorrect=1&api_key=894064fca12d26335a68f014d98f4145&format=json"
+  }).done(function(data){
+		console.log(data);
+		if (data.error){
+			$("#notice").append(data.message+".");
+		} else if (data.events.total === "0"){
+			$("#notice").append("Sorry, no shows found at this time.");
+		} else if (data.events["@attr"].total === "1"){	
+			displaySingle(data.events.event, artist);
+		} else if (data.events["@attr"].artist === artist) {
+			displayData(data.events.event, artist);
+		} else {
+			$("#notice").append("Did you mean '"+data.events["@attr"].artist+
+			"', ya dingus?<br /><button id='autocorrect-correct' class='btn'>Yup, show me "+
+			data.events["@attr"].artist+
+			".</button><button id='autocorrect-incorrect' class='btn'>Nope, I meant "+
+			origArtist+".</button>");
+			$("#autocorrect-correct").click(function(){
+				displayData(data.events.event, data.events["@attr"].artist);
+			});
+			$("#autocorrect-incorrect").click(function(){
+				$.ajax({
+			    url: "http://ws.audioscrobbler.com/2.0/?method=artist.getevents&artist="+origArtist+"&autocorrect=0&api_key=894064fca12d26335a68f014d98f4145&format=json"
+			  }).done(function(data){
+					if (data.events.total === "0"){
+						$("#notice").html("");
+						$("#artist-name").html("");
+						$("#notice").append("Sorry, no shows found at this time.");
+					} else {
+						displayData(data.events.event, origArtist);
+					}
+				});
+			});
+		}
+  });
+}
+
+function initialize(data) {
+  $('#map_canvas').slideDown();
+  console.log(data);
+  // for (var i = 0; i < data.length; i++) {    
+    var locationLat = data[0].venue.location['geo:point']['geo:lat'];
+    var locationLong = data[0].venue.location['geo:point']['geo:long'];
+  //}
+  console.log(location);
+  var myOptions = {
+    center: new google.maps.LatLng(locationLat, locationLong),
+    zoom: 5,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
+}
+
+
+function displayData(data, artist) {
+	$("#notice").html("");
+	$("#artist-name").html("");
+	$("#artist-name").append(artist);
+	for (var i = 0; i < data.length; i++) {
+    $('#shows').append('<li id='+i+'>'+data[i].startDate+" at "+data[i].venue.name+" in "+
+		data[i].venue.location.city+ ", "+data[i].venue.location.country+" </li>");
+  }
+	$("#shows li:last").addClass("last");
+	sortData(data);
+	$('#map-button').append("Do you want to see a map for this? <br /><button id='submit' class='btn'>VIEW MAP</button>");
+	$('#map-button .btn').click( function(){
+	  initialize(data);
+	});
+}
+
+function displaySingle(data, artist) {
+	$("#notice").html("");
+	$("#artist-name").html("");
+	$("#artist-name").append(artist);
+	console.log(data);
+  $('#shows').append('<li id="1">'+data.startDate.substring(0,16)+" at "+data.venue.name+" in "+
+	data.venue.location.city+ ", "+data.venue.location.country+" </li>");
+	$("#shows li:last").addClass("last");
+	sortData(data);
+}
+
+function toTitleCase(str){
+	return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
+function sortData(data){
+	for (var j = 0; j < data.length; j++) {
+		if (data[j].cancelled === "1"){
+			$('#'+j).append("<span class='cancelled'> Cancelled!</span>");
+		}
+	}
+}
+
 $(function() {
 	var availableTags = [
 	"Coldplay",
@@ -1012,93 +1114,3 @@ $(function() {
 		source: availableTags
 	});
 });
-
-function getData() {
-	$(".ui-autocomplete").hide();
-	$("header").slideUp();    
-  $('#artist-name').html("");
-	$('#notice').html("");
-  $('#shows').html("");
-	var origArtist = $('#search-artist input').val();
-  var artist = toTitleCase(origArtist);
-  $.ajax({
-    url: "http://ws.audioscrobbler.com/2.0/?method=artist.getevents&artist="+artist+"&autocorrect=1&api_key=894064fca12d26335a68f014d98f4145&format=json"
-  }).done(function(data){
-		console.log(data);
-		if (data.error){
-			$("#notice").append(data.message+".");
-		} else if (data.events.total === "0"){
-			$("#notice").append("Sorry, no shows found at this time.");
-		} else if (data.events["@attr"].total === "1"){	
-			displaySingle(data.events.event, artist);
-		} else if (data.events["@attr"].artist === artist) {
-			displayData(data.events.event, artist);
-		} else {
-			$("#notice").append("Did you mean '"+data.events["@attr"].artist+
-			"', ya dingus?<br /><button id='autocorrect-correct' class='btn'>Yup, show me "+
-			data.events["@attr"].artist+
-			".</button><button id='autocorrect-incorrect' class='btn'>Nope, I meant "+
-			origArtist+".</button>");
-			$("#autocorrect-correct").click(function(){
-				displayData(data.events.event, data.events["@attr"].artist);
-			});
-			$("#autocorrect-incorrect").click(function(){
-				$.ajax({
-			    url: "http://ws.audioscrobbler.com/2.0/?method=artist.getevents&artist="+origArtist+"&autocorrect=0&api_key=894064fca12d26335a68f014d98f4145&format=json"
-			  }).done(function(data){
-					if (data.events.total === "0"){
-						$("#notice").html("");
-						$("#artist-name").html("");
-						$("#notice").append("Sorry, no shows found at this time.");
-					} else {
-						displayData(data.events.event, origArtist);
-					}
-				});
-			});
-		}
-  });
-}
-
-function initialize(data) {
-  console.log(data);
-  // for (var i = 0; i < data.length; i++) {    
-    var locationLat = data[0].venue.location['geo:point']['geo:lat'];
-    var locationLong = data[0].venue.location['geo:point']['geo:long'];
-  //}
-  console.log(location);
-  var myOptions = {
-    center: new google.maps.LatLng(locationLat, locationLong),
-    zoom: 5,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  var map = new google.maps.Map(document.getElementById("map_canvas"), myOptions);
-}
-
-
-function displayData(data, artist) {
-	$("#notice").html("");
-	$("#artist-name").html("");
-	$("#artist-name").append(artist);
-	for (var i = 0; i < data.length; i++) {
-    $('#shows').append('<li id='+i+'>'+data[i].startDate+" at "+data[i].venue.name+" in "+
-		data[i].venue.location.city+ ", "+data[i].venue.location.country+" </li>");
-  }
-	$("#shows li:last").addClass("last");
-	sortData(data);
-	$('#map-button').append("Do you want to see a map for this? <br /><button id='submit' class='btn'>VIEW MAP</button>");
-	$('#map-button .btn').click( function(){
-	  initialize(data);
-	});
-}
-
-function toTitleCase(str){
-	return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-}
-
-function sortData(data){
-	for (var j = 0; j < data.length; j++) {
-		if (data[j].cancelled === "1"){
-			$('#'+j).append("<span class='cancelled'> Cancelled!</span>");
-		}
-	}
-}
